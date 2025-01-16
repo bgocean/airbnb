2975c54fe7ac737b9507f24bae7e800ed44bb692
bgocean <bigocean700@gmail.com>
2025-01-16 23:49:52 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+// https://www.youtube.com/live/np5bD3YU1Fg 
+// 30.30
